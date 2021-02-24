b55b2ca83b721e378604387c1327ecb795fc94ed
gabriel <boghiangabriel96@yahoo.com>
2021-02-24 16:25:47 +0200

    [Lista Autoturisme]-[ADD] CheckBox Select/Deselect (Tot) din lista de autoturisme

--- WindowsApplication1/Lista Autoturisme/ListaAuto.vb
+++ WindowsApplication1/Lista Autoturisme/ListaAuto.vb
@@ -61,4 +61,81 @@ Public Class ListaAuto
         Afisare_Auto()
     End Sub
 #End Region
+
+    Private Sub MaterialRaisedButton2_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton2.Click
+
+    End Sub
+
+    Private Sub MaterialRaisedButton3_Click(sender As Object, e As EventArgs) Handles MaterialRaisedButton3.Click
+        If chklstbox_Auto.CheckedItems.Count = 0 Then
+            MessageBox.Show("Te rugam sa selectati cel putin o optiune din lista de mai sus!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+            '[ADD] - sterge lista si o reactualizeaza
+            lstBoxAuto.Items.Clear()
+            For Each item In chklstbox_Auto.CheckedItems
+                lstBoxAuto.Items.Add(item)
+            Next
+            MessageBox.Show("Informatiile au fost adaugate cu succes!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        End If
+
+        ' Atunci cand adaug un autoturism in lista de favorite, in lista principale mi le pune ca au fost deja introduse
+        ' verificare pentru a nu putea adauga de 2 ori acelasi autoturism in lista de favorite
+        ' GetItemChecked - returneaza exact cate iteme au fost bifate - 
+        ' Returns a value indicating whether the specified item Is checked. Return TRUE / FALSE
+        ' REZOLVARE https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.checkedlistbox.getitemchecked?view=netframework-4.5.2&f1url=%3FappId%3DDev14IDEF1%26l%3DEN-US%26k%3Dk(System.Windows.Forms.CheckedListBox.GetItemChecked);k(TargetFrameworkMoniker-.NETFramework,Version%253Dv4.5.2);k(DevLang-VB)%26rd%3Dtrue
+
+
+        For i As Integer = 0 To chklstbox_Auto.Items.Count - 1
+            If (chklstbox_Auto.GetItemChecked(i)) Then
+                chklstbox_Auto.SetItemCheckState(i, CheckState.Indeterminate)
+            End If
+        Next
+    End Sub
+
+    Private Sub MaterialCheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles MaterialCheckBox1.CheckedChanged
+        If chklstbox_Auto.Items.Count <> 0 Then
+
+            For i As Integer = 0 To chklstbox_Auto.Items.Count - 1 Step 1
+                If MaterialCheckBox1.Checked = True Then
+                    chklstbox_Auto.SetItemChecked(i, True)
+                Else
+                    chklstbox_Auto.SetItemChecked(i, False)
+                End If
+            Next
+            'MessageBox.Show("nr total este: " & chklstbox_Auto.Items.Count)
+
+        Else
+            VerificCheckBoxDacaEsteBifatIlDebifezCuMesajDeEroare()
+            'MessageBox.Show("Pentru a putea selecta toate informatiile, ar trebui ca sa afisati informatiile apasand butonul de mai jos!", "Informatie", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+#Region "Verific daca CheckBox-ul este bifat, atunci il debifez"
+    Private Sub VerificCheckBoxDacaEsteBifatIlDebifez()
+        If MaterialCheckBox1.Checked = True Then
+            MaterialCheckBox1.Checked = False
+        End If
+    End Sub
+
+#End Region
+
+#Region "Verific daca CheckBox-ul este bifat, atunci il debifez cu mesaj de eroare"
+    Private Sub VerificCheckBoxDacaEsteBifatIlDebifezCuMesajDeEroare()
+        If MaterialCheckBox1.Checked = True Then
+            MaterialCheckBox1.Checked = False
+        Else
+            MessageBox.Show("Pentru a putea selecta toate informatiile, ar trebui ca sa afisati informatiile apasand butonul de mai jos!", "Informatie", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+
+        End If
+    End Sub
+
+#End Region
+
+#Region "Stergere info din textBox"
+    Private Sub ClearTxtBox()
+        txtMarca.Text = ""
+        txtModel.Text = ""
+    End Sub
+#End Region
 End Class
